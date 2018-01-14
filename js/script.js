59eaefbf58452bdf8904d743d04b5bdaa865c9c0
Drew Mercer <japanexpress@gmail.com>
2018-01-14 14:22:27 -0500

    Updating to use API for geolocation

--- js/script.js
+++ js/script.js
@@ -4,51 +4,58 @@ $(document).ready(function() {
   var fTemp;
   var cTemp;
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      $('#data').html('latitude: ' + lat + '<br>longitude: ' + long);
+  $.getJSON('http://ip-api.com/json', function(data2) {
+    lat = data2.lat;
+    long = data2.lon;
 
-      var api =
-        'http://api.openweathermap.org/data/2.5/weather?lat=' +
-        lat +
-        '&lon=' +
-        long +
-        '&appid=5b22857b72f63301742fd17687a5d009';
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //       long = position.coords.longitude;
+    //       lat = position.coords.latitude;
+    //       $('#data').html('latitude: ' + lat + '<br>longitude: ' + long);
 
-      $.getJSON(api, function(data) {
-        console.log(data.coord.lon);
+    var api =
+      'http://api.openweathermap.org/data/2.5/weather?lat=' +
+      lat +
+      '&lon=' +
+      long +
+      '&appid=5b22857b72f63301742fd17687a5d009';
 
-        var weatherType = data.weather[0].description;
-        var windSpeed = data.wind.speed;
+    $.getJSON(api, function(data) {
+      console.log(data.coord.lon);
 
-        var kTemp = data.main.temp;
-        fTemp = (kTemp * (9 / 5) - 459.67).toFixed(0);
-        cTemp = (kTemp - 273).toFixed(1);
-        var tempToggle = true;
+      var weatherType = data.weather[0].description;
+      var windSpeed = (data.wind.speed * 2.237).toFixed(1);
 
-        var city = data.name;
-        console.log(city);
-        console.log(api);
+      var kTemp = data.main.temp;
+      fTemp = (kTemp * (9 / 5) - 459.67).toFixed(0);
+      cTemp = (kTemp - 273).toFixed(1);
+      var tempToggle = true;
 
-        $('#city').html(city);
-        $('#weatherType').html(weatherType);
-        $('#fTemp').html(fTemp + '&#8457;');
-        $('#fTemp').click(function() {
-          if ((tempToggle === false)) {
-            
-            $('#fTemp').html(fTemp + '&#8457;');
-            tempToggle = true;
-          } else {
-            $('#fTemp').html(cTemp + '&#8451;');
-            tempToggle = false;
-          }
-        });
-        $('#windSpeed').html(windSpeed);
-      }).catch(function(error) {
-        console.log('Error caught: ' + error);
+      var city = data.name;
+      console.log(city);
+      console.log(api);
+
+      $('#city').html(city);
+      $('#weatherType').html(weatherType);
+      $('#fTemp').html(fTemp + '&#8457;');
+      $('#fTemp').click(function() {
+        if (tempToggle === false) {
+          $('#fTemp').html(fTemp + '&#8457;');
+          tempToggle = true;
+        } else {
+          $('#fTemp').html(cTemp + '&#8451;');
+          tempToggle = false;
+        }
       });
+      $('#windSpeed').html(windSpeed + ' mph');
+
+      if (fTemp > 80) {
+        $('body').css('background-image', 'url()');
+      } else if (fTemp > 70) {
+      }
+    }).catch(function(error) {
+      console.log('Error caught: ' + error);
     });
-  }
+  });
 });
