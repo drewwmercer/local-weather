74e6c59041e0de480570b5cc709f00349fa33b8f
Drew Mercer <japanexpress@gmail.com>
2018-01-14 12:51:23 -0500

    Pulling in position with geolocation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
 $(document).ready(function(){
+    var long;
+    var lat;
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            $('#data').html('latitude: ' + lat + '<br>longitude: ' + long);
+        })
+    }
+
 
     var api = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5b22857b72f63301742fd17687a5d009';
 
