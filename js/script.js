200dcfec0374b3cdc7e3687e54f3f91c431201de
Drew Mercer <japanexpress@gmail.com>
2018-01-14 12:43:28 -0500

    Adding my API key

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-    var api = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22';
+    var api = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5b22857b72f63301742fd17687a5d009';
 
     $.getJSON(api,function(data){
         console.log(data.coord.lon);
